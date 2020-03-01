2e4c01e6a844303f41722f8dcf430477626d0159
Buddy Lim <buddy.tlimk@gmail.com>
2020-03-01 17:33:53 +0800

    Initial implementation of sorting in comic titles A-Z & Z-A - Lim

--- src/component/titleList.js
+++ src/component/titleList.js
@@ -2,26 +2,56 @@ import React from 'react';
 import Header from "./header";
 import Banner from "./banner";
 import "./css/titleList.css";
+import axios from 'axios';
+
+import {BrowserRouter as Route, Link } from "react-router-dom";
+
+import ComicDetails from "./comicDetails";
 
 export default class TitleList extends React.Component {
     constructor(props) { //holds data to be parsed into the page
         super(props);
+        this.handleChange = this.handleChange.bind(this);
         this.state = {
-            poster: "https://placehold.it/150x80?text=IMAGE",
-            link: "/comic-details",
-            title: "Comic Title",
-            chapter: "XXX"
+            comic : [],
+            alphabetical : 'az'
         };
     }
+
+    
+    componentDidMount() {
+        axios.get('http://localhost:4000/on9comics/comic')
+            .then(response => {
+                this.setState({ comic : response.data });
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    handleClick(e){
+        // access to e.target here
+        // console.log(e.currentTarget);
+    }
+
+
     createComic = () => { //creates the list of comics to be shown in the page
         let comic = []
-        for (let j = 0; j < 5; j++) {
-            comic.push(<div class="col-sm-3"><img src={this.state.poster} class="img-responsive" style={{width:"100%"}} alt="Image"></img>
-            <a href={this.state.link} class="comicTitle">{this.state.title}</a>
-            <p class="comicChapter">Ch.{this.state.chapter}</p></div>)
+        for (let i = 0; i < this.state.comic.length; i++) { 
+            comic.push(
+                <div class="col-sm-4">
+                    <Link to ={"/comic/" + this.state.comic[i]._id}>
+                        <img src={this.state.comic[i].filepath} class="img-responsive titleListImg" onClick={this.handleClick.bind(this.state.comic[i])} style={{width:"100%"}} alt="comic"></img>
+                        <p class="comicTitle">{this.state.comic[i].title}</p>
+                        <p class="comicChapter">Ch.{this.state.comic[i].latestchapter}</p>
+                    </Link>
+                </div>
+            )
         }
+
         return comic
     }
+
     createTitleCaseList = (charA,charZ) => { //creates the alphabet list in the filter system
         let a = []
         let i = charA.charCodeAt(0)
@@ -33,7 +63,33 @@ export default class TitleList extends React.Component {
         }
         return a;
     }
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.type === 'select' ? target.selected : target.value;
+        // const alphabetical = value;
+        
+        console.log(value)
+
+        this.setState({
+            alphabetical : value }
+    );
+    }
+
     render() {
+        let sortedTitles;
+
+        if (this.state.alphabetical === "az") {
+            console.log("sort");
+            sortedTitles = this.state.comic.sort((a, b) =>
+              a.title > b.title ? 1 : -1
+            );
+          } else {
+            sortedTitles = this.state.comic.sort((a, b) =>
+              a.title < b.title ? 1 : -1
+            );
+          }
+
         return (
         <div>
             <Header/>
@@ -48,18 +104,15 @@ export default class TitleList extends React.Component {
                     <div class="col-sm-4" id="filterContainer">
                         <div class="filterBox">
                             <div class="title">Sort By</div>
-                            <form action="/">
                                 <div class="form-group">
                                     <label for="sel1">Sort by:</label>
-                                    <select class="form-control" id="sel1">
-                                        <option val="">Alphabetical order</option>
-                                        <option val="">Latest update</option>
-                                        <option val="">Released most recent</option>
+                                    <select value={this.state.sort} onChange={this.handleChange} class="form-control" id="sel1">
+                                        <option selected value="az">Sort A-Z</option>
+                                        <option value="za">Sort Z-A</option>
+                                        {/* <option selected val="">Latest update</option>
+                                        <option val="">Released most recent</option> */}
                                     </select>
                                 </div>
-                                <button type="reset" class="btn btn-default">Default</button>
-                                <button type="submit" class="btn btn-default">Sort</button>
-                            </form>
                         </div>
                         <div class="filterBox">
                             <div class="title">Filter By</div>
@@ -91,6 +144,7 @@ export default class TitleList extends React.Component {
                     </div>
                 </div>
             </div>
+            <Route path ="/comic/:id" component={ComicDetails}/>
         </div>);
     }
 }
